ded98edc8d046983f866d7996911f8676636a0ac
Connor Abbas <abbasconnor@gmail.com>
2026-08-24 20:59:02 -0500

    chore(nuxt): upgrade app to Nuxt 4

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,11 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2024-11-01',
-    devtools: { enabled: true },
-    devServer: {
-        port: process.env.NUXT_DEV_SERVER_PORT || 3000,
-    },
-    runtimeConfig: {
-        public: {},
-    },
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
 })
