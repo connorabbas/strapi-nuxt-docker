ddceaf010615420ecf8b2dcc238f4a39c16de889
Connor Abbas <abbasconnor@gmail.com>
2026-08-29 14:03:53 +0000

    feat: add Nuxt UI and ESLint

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,9 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  runtimeConfig: {
-    strapiUrl: 'http://strapi:1337',
-    strapiApiToken: ''
-  }
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/ui'
+    ],
+    devtools: { enabled: true },
+    css: ['~/assets/css/main.css'],
+    runtimeConfig: {
+        strapiUrl: 'http://strapi:1337',
+        strapiApiToken: ''
+    },
+    compatibilityDate: '2025-07-15',
+    eslint: {
+        config: {
+            stylistic: {
+                commaDangle: 'never',
+                braceStyle: '1tbs'
+            }
+        }
+    }
 })
